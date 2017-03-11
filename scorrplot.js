705c0ae6687a0caeadf883938c800532aef4105c
Sean McKenna <mckennapsean@gmail.com>
2017-03-10 17:38:06 -0700

    add some more vector computation

--- scorrplot.js
+++ scorrplot.js
@@ -1,6 +1,7 @@
 // global variables for testing
 var dataset
 var vectors = []
+var svectors = []
 
 d3.csv('data/test.csv', (e, d) => {
   console.log('data loaded')
@@ -22,6 +23,20 @@ d3.csv('data/test.csv', (e, d) => {
   console.log('vectors computed')
 
   // standardize vectors (mean-centered, scaled)
+  for (let i = 0; i < vectors.length; i++) {
+    // compute mean
+    let mean = 0
+    for (let j = 0; j < vectors[0].length; j++) {
+      mean += vectors[i][j]
+    }
+    mean = mean / vectors[0].length
+    // subtract mean
+    for (let j = 0; j < vectors[0].length; j++) {
+      svectors[i][j] = vector[i][j] - mean;
+    }
+    // scale length to 1
+    for (let j = 0; j < vectors[0].length; j++) {}
+  }
   console.log('data standardized')
 
   // select primary & secondary points of interest
