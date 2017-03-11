df4b2b6ee0ece261886b5e83607a469e9381bd48
Sean McKenna <mckennapsean@gmail.com>
2017-03-10 17:24:27 -0700

    store data and simplified vectors

--- scorrplot.js
+++ scorrplot.js
@@ -1,5 +1,22 @@
+var dataset
+var vectors = []
 
 d3.csv('data/test.csv', (e, d) => {
   console.log('data loaded')
   // console.log(d)
+
+  // store data in browser
+  dataset = d
+
+  // figure out how many dimensions of data
+  let dimensions = Object.keys(dataset[0]).length - 2
+
+  // compute vectors and store them
+  for (let row in dataset) {
+    let vector = []
+    for (let i = 0; i < dimensions; i++) {
+      vector[i] = dataset[row]['X' + (i + 1)]
+    }
+    vectors[row] = vector
+  }
 })
